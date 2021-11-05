82ac2408b70467e414df4470052419a1cd6683d3
Andrea <andr3z45@gmail.com>
2021-11-05 15:50:34 +0100

    prompt + calcolo prezzo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let myKm = parseFloat(prompt('inserisci KM da percorrere'));
+console.log(myKm);
+
+let myAge = parseInt(prompt('inserisci la tua età'));
+console.log(myAge);
+
+let prezzo = myKm * 0.21;
+console.log(prezzo);
