7f5caf7f8dd809d73e1ae3b0617e5d12f822a7c3
Andrea <andr3z45@gmail.com>
2021-11-05 16:37:59 +0100

    sconti + prezzo finale

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ console.log(myAge);
 let prezzo = myKm * 0.21;
 console.log(prezzo);
 
-let sconto = 
+let sconto = 0;
+if (myAge < 18){
+    sconto = prezzo * 20 / 100
+}
+if (myAge > 65){
+    sconto = prezzo * 40 / 100
+}
+console.log(sconto);
+
+let prezzoFinale = prezzo - sconto 
+console.log(prezzoFinale);
